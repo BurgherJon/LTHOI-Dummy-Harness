3dd46c27d1d55ba086437c4f6b66ea61522583af
BurgherJon <JonathanCavell@gmail.com>
2016-09-07 15:59:01 -0700

    Updated to include a simple main.html that makes a webservice call.

--- src/main/java/com/lthoi/test/Constants.java
+++ src/main/java/com/lthoi/test/Constants.java
@@ -4,7 +4,7 @@ package com.lthoi.test;
  * Contains the client IDs and scopes for allowed clients consuming your API.
  */
 public class Constants {
-  public static final String WEB_CLIENT_ID = "replace this with your web client ID";
+  public static final String WEB_CLIENT_ID = "989392916115-46vtl5fifcm5mso17iffrmh6lt2ke48s.apps.googleusercontent.com";
   public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
   public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
   public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
